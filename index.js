1170629b69d7b8aed3bf10896d9fc4428c540889
Whelpley <whelpley@gmail.com>
2016-09-12 09:35:16 -0400

    Send back generic cards

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ app.post('/webhook/', function (req, res) {
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
     }
